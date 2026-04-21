a4c359a76fbad7d1308774e870b90fe821374873
Melis Kara <meliskara-dev@.com>
2026-04-21 17:37:42 +0300

    Fix: nested repository issue and cached files

---
+++ client/src/components/ShippingForm.tsx
@@ -0,0 +1,118 @@
+import { ShippingFormInputs, shippingFormSchema } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const ShippingForm = ({
+  setShippingForm
+}: {
+  setShippingForm: (data: ShippingFormInputs) => void;
+}) => {
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ShippingFormInputs>({
+    resolver: zodResolver(shippingFormSchema),
+  });
+
+  const router = useRouter();
+
+  const handleSubmitShippingForm: SubmitHandler<ShippingFormInputs> = (
+    data,
+  ) => {
+    setShippingForm(data);
+    router.push("/cart?step=3", { scroll: false });
+  };
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(handleSubmitShippingForm)}>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="name" className="text-sm text-gray-500 font-medium">
+          Name
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="name"
+          placeholder="Melis Kara"
+          {...register("name")}
+        />
+        {errors.name && (
+          <p className="text-sm text-red-500">{errors.name.message}</p>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email" className="text-sm text-gray-500 font-medium">
+          E-Mail
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="email"
+          id="email"
+          placeholder="meliskara.dev@gmail.com"
+          {...register("email")}
+        />
+        {errors.email && (
+          <p className="text-sm text-red-500">{errors.email.message}</p>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="phone" className="text-sm text-gray-500 font-medium">
+          Phone
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="phone"
+          placeholder="1234567890"
+          {...register("phone")}
+        />
+        {errors.phone && (
+          <p className="text-sm text-red-500">{errors.phone.message}</p>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="address" className="text-sm text-gray-500 font-medium">
+          Address
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="address"
+          placeholder="Kordonboyu Mah. Kartal"
+          {...register("address")}
+        />
+        {errors.address && (
+          <p className="text-sm text-red-500">{errors.address.message}</p>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="city" className="text-sm text-gray-500 font-medium">
+          City
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="city"
+          placeholder="Istanbul"
+          {...register("city")}
+        />
+        {errors.city && (
+          <p className="text-sm text-red-500">{errors.city.message}</p>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2"
+      >
+        Continue
+        <ArrowRight className="w-3 h-3" />
+      </button>
+    </form>
+  );
+};
+
+export default ShippingForm;
